ffc61839f124d2d5e3a1fbeb9708b85a57c41174
Admin <Admin@DESKTOP-7T0EOMO>
2024-01-25 09:31:03 +0530

    changes in contact pages added details page delete option received messages viewing option and some more..

---
+++ BestShop/Myhelper/EmailSender.cs
@@ -0,0 +1,23 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace BestShop.Myhelper
+{
+    public class EmailSender
+    {
+        public static async Task SendEmail(string toEmail,string username,string subject
+            ,string message)
+        {
+            string apiKey = "";
+            var client=new SendGridClient(apiKey);
+
+            var from=new EmailAddress("manideep.n15@gmail.com","BestShop.com");
+            var to = new EmailAddress(toEmail, username);
+            var plainTextContent = message;
+            var htmlContent = "";
+
+            var msg = MailHelper.CreateSingleEmail(from,to,subject,plainTextContent,htmlContent);
+            var response = await client.SendEmailAsync(msg);
+        }
+    }
+}
--- BestShop/Pages/Contact.cshtml.cs
+++ BestShop/Pages/Contact.cshtml.cs
@@ -1,5 +1,10 @@
+using BestShop.Myhelper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
 using System.Diagnostics.SymbolStore;
 
 namespace BestShop.Pages
@@ -9,36 +14,108 @@ namespace BestShop.Pages
         public void OnGet()
         {
         }
+       
+        [Required(ErrorMessage="The First Name is required")]
+        [Display(Name ="First Name")]
         public string FirstName { get; set; } = "";
+
+        [BindProperty,Required(ErrorMessage="The Last Name is required")]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; } = "";
+
+        [BindProperty,Required(ErrorMessage="The Email is required")]
+        [EmailAddress]
+        [Display(Name = "Email")]
         public string Email { get; set; } = "";
+
+        [BindProperty]
+        [RegularExpression("^[a-zA-Z]*$", ErrorMessage = "Only Alphabets allowed.")]
         public string Phone { get; set; } = "";
+
+        [BindProperty,Required]
+        [Display(Name = "Subject")]
         public string Subject { get; set; } = "";
+
+
+        [BindProperty,Required(ErrorMessage="The Message is required")]
+        [MinLength(5,ErrorMessage="the Message should be at least 5 characters")]
+        [MaxLength(1024,ErrorMessage="the Message should be less 1024 characters")]
+        [Display(Name ="Message")]
         public string Message { get; set; } = "";
+
+        public List<SelectListItem> SubjectList { get; }=new List<SelectListItem>
+        {
+            new SelectListItem{Value="Order Status",Text="Order Status"},
+            new SelectListItem{Value="Refund Request",Text="Refund Request"},
+            new SelectListItem{Value="Job Application",Text="Job Application"},
+            new SelectListItem{Value="Other",Text="Other"},
+         };  
 
         public string SuccessMessage { get; set; } = "";
         public string ErrorMessage { get; set; } = "";
         public void OnPost()
         {
-            FirstName = Request.Form["firstname"];
-            LastName = Request.Form["lastname"];
-            Email = Request.Form["email"];
-            Phone = Request.Form["phone"];
-            Subject = Request.Form["subject"];
-            Message = Request.Form["message"];
+
 
             //check if any required field is empty
-            if (FirstName.Length == 0 || LastName.Length == 0 || Email.Length == 0 || 
-                Subject.Length==0 || Message.Length==0)
+            if (!ModelState.IsValid)
             {
                 //error
                 ErrorMessage = "Please fill all required fields";
                 return; 
             }
+            if (Phone == null) Phone = "";
+
             // add this message to database
+            try
+            {
+                string connectionString = "Data Source=DESKTOP-7T0EOMO;Initial Catalog=bestshop;Integrated Security=True;";
+                using(SqlConnection connection=new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sql = "INSERT INTO messages " +
+                        "(firstname,lastname,email,phone,subject,message) VALUES " +
+                        "(@firstname,@lastname,@email,@phone,@subject,@message);";
+                    using(SqlCommand cmd=new SqlCommand(sql,connection))
+                    {
+                        cmd.Parameters.AddWithValue("@firstname",FirstName);
+                        cmd.Parameters.AddWithValue("@lastname", LastName);
+                        cmd.Parameters.AddWithValue("@email", Email);
+                        cmd.Parameters.AddWithValue("@phone", Phone);
+                        cmd.Parameters.AddWithValue("@subject", Subject);
+                        cmd.Parameters.AddWithValue("@message", Message);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+             }
+            catch(Exception ex)
+            {
+                ErrorMessage=ex.Message;
+                return;    
+            }
 
             //send confirmation message to client
+            string username = FirstName + " " + LastName;
+            string emailSubject = "About your message";
+            string emailMessage = "Dear " + username + ",\n" +
+                "We received your message.Thank you for contacting us..\n" +
+                "our team will contact you soon.\n" +
+                "Best Regards\n\n" +
+                "Your message:\n" + Message;
+
+            EmailSender.SendEmail(Email, username, emailSubject, emailMessage).Wait();
+     
             SuccessMessage = "Your message has been recieved correctly..";
+
+            FirstName = "";
+            LastName = "";
+            Email = "";
+            Phone = "";
+            Subject = "";
+            Message = "";
+
+            ModelState.Clear();
         }
     }
 }
