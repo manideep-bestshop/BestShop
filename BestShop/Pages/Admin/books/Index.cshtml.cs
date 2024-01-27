bb080868c2d1c94397131bf378473b201dd55d05
Admin <Admin@DESKTOP-7T0EOMO>
2024-01-27 21:38:25 +0530

    pagination to books sort functionaity on books inserting new books

--- BestShop/Pages/Admin/books/Index.cshtml.cs
+++ BestShop/Pages/Admin/books/Index.cshtml.cs
@@ -8,46 +8,103 @@ namespace BestShop.Pages.Admin.books
     {
         public List<BookInfo> listBooks = new List<BookInfo>();
         public string search = "";
+
+        public int page = 1; // the current html page
+        public int totalPages = 0;
+        private readonly int pageSize = 5; // books per page
+
+        public string column = "id";
+        public string order = "desc";
+
+
+
         public void OnGet()
         {
             search = Request.Query["search"];
-            if (search == null) search = " ";
+            if (search == null) search = "";
+
+            page = 1;
+            string requestPage = Request.Query["page"];
+            if (requestPage != null)
+            {
+                try
+                {
+                    page = int.Parse(requestPage);
+                }
+                catch (Exception ex)
+                {
+                    page = 1;
+                }
+            }
+
+            string[] validColumns = { "id", "title", "authors", "num_pages", "price", "category", "created_at" };
+            column = Request.Query["column"];
+            if (column == null || !validColumns.Contains(column))
+            {
+                column = "id";
+            }
+
+            order = Request.Query["order"];
+            if (order == null || !order.Equals("asc"))
+            {
+                order = "desc";
+            }
+
             try
             {
-                string connectionString = "Data Source=DESKTOP-7T0EOMO;Initial Catalog=bestshop;Integrated Security=True;";
+                string connectionString = "Data Source=DESKTOP-7T0EOMO;database=bestshop;Integrated Security=True;";
 
-                using(SqlConnection connection = new SqlConnection(connectionString))
+                using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
-                    string sql = "SELECT * FROM books";
-                    if(search.Length>0)
+
+                    string sqlCount = "SELECT COUNT(*) FROM books";
+                    if (search.Length > 0)
                     {
-                        sql += "WHERE title LIKE @search OR authors LIKE @search";
+                        sqlCount += " WHERE title LIKE @search OR authors LIKE @search";
                     }
-                    sql+="ORDER BY id DESC";
-                    using(SqlCommand command=new SqlCommand(sql,connection))
+
+                    using (SqlCommand command = new SqlCommand(sqlCount, connection))
                     {
                         command.Parameters.AddWithValue("@search", "%" + search + "%");
-                        using(SqlDataReader reader = command.ExecuteReader()) 
-                        { 
-                             while(reader.Read()) 
-                            { 
+
+                        decimal count = (int)command.ExecuteScalar();
+                        totalPages = (int)Math.Ceiling(count / pageSize);
+                    }
+
+                    string sql = "SELECT * FROM books";
+                    if (search.Length > 0)
+                    {
+                        sql += " WHERE title LIKE @search OR authors LIKE @search";
+                    }
+                    sql += " ORDER BY " + column + " " + order; //" ORDER BY id DESC";
+                    sql += " OFFSET @skip ROWS FETCH NEXT @pageSize ROWS ONLY";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@search", "%" + search + "%");
+                        command.Parameters.AddWithValue("@skip", (page - 1) * pageSize);
+                        command.Parameters.AddWithValue("@pageSize", pageSize);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
                                 BookInfo bookInfo = new BookInfo();
                                 bookInfo.Id = reader.GetInt32(0);
-                                bookInfo.Title=reader.GetString(1); 
-                                bookInfo.Authors=reader.GetString(2); 
-                                bookInfo.Isbn=reader.GetString(3);
+                                bookInfo.Title = reader.GetString(1);
+                                bookInfo.Authors = reader.GetString(2);
+                                bookInfo.Isbn = reader.GetString(3);
                                 bookInfo.NumPages = reader.GetInt32(4);
-                                bookInfo.Price=reader.GetDecimal(5);
+                                bookInfo.Price = reader.GetDecimal(5);
                                 bookInfo.Category = reader.GetString(6);
                                 bookInfo.Description = reader.GetString(7);
-                                bookInfo.ImageFileName=reader.GetString(8);
+                                bookInfo.ImageFileName = reader.GetString(8);
                                 bookInfo.CreatedAt = reader.GetDateTime(9).ToString("MM/dd/yyyy");
 
-                                listBooks.Add(bookInfo);    
+                                listBooks.Add(bookInfo);
                             }
                         }
-
                     }
                 }
             }
