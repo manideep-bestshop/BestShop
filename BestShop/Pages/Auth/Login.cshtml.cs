ac0ac5ffe12c741ec78fadbffa69e20a91329872
Admin <Admin@DESKTOP-7T0EOMO>
2024-01-31 17:10:14 +0530

    created Login and Register forms and given authentications to it..

---
+++ BestShop/Pages/Auth/Login.cshtml.cs
@@ -0,0 +1,109 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
+
+namespace BestShop.Pages.Auth
+{
+    [BindProperties]
+    public class LoginModel : PageModel
+    {
+        [Required(ErrorMessage ="The Email is Required"),EmailAddress]
+        public string Email { get; set; } = "";
+
+        [Required(ErrorMessage ="Password is required")]
+        public string Password { get; set; } = "";
+
+        public string errorMessage = "";
+        public string successMessage = "";
+
+		public override void OnPageHandlerExecuting(PageHandlerExecutingContext context)
+		{
+			base.OnPageHandlerExecuting(context);
+
+            if (HttpContext.Session.GetString("role") != null)
+            {
+                //the user is already authenticated => redirect the user to home page
+                context.Result = new RedirectResult("/");
+            }
+		}
+		public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+          if(!ModelState.IsValid)
+            {
+                errorMessage = "Data validation failed";
+                return;
+            }
+            //successfull data validation
+
+            //connect to data base and check the users credentials
+            try
+            {
+                string connectionString = "Data Source=DESKTOP-7T0EOMO;database=bestshop;Integrated Security=True;";
+                using(SqlConnection connection=new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sql = "SELECT * FROM users WHERE email=@email";
+                    
+                    using(SqlCommand command=new SqlCommand(sql,connection))
+                    {
+                        command.Parameters.AddWithValue("@email", Email);
+                        using(SqlDataReader reader=command.ExecuteReader())
+                        {
+                            if(reader.Read())
+                            {
+                                int id = reader.GetInt32(0);
+                                string firstname=reader.GetString(1);
+                                string lastname=reader.GetString(2);    
+                                string email=reader.GetString(3);
+                                string phone = reader.GetString(4);
+                                string address = reader.GetString(5);
+                                string hashedPassword=reader.GetString(6);
+                                string role = reader.GetString(7);
+                                string created_at = reader.GetDateTime(8).ToString("MM/dd/yyyy");
+
+                                // verify password
+                                var passwordHasher = new PasswordHasher<IdentityUser>();
+                                var result = passwordHasher.VerifyHashedPassword(new IdentityUser(),
+                                    hashedPassword, Password);
+
+                                if(result==PasswordVerificationResult.Success
+                                    || result==PasswordVerificationResult.SuccessRehashNeeded)
+                                {
+                                    // successfull password verification => initialize the session
+                                    HttpContext.Session.SetInt32("id", id);
+                                    HttpContext.Session.SetString("firstname", firstname);
+                                    HttpContext.Session.SetString("lastname", lastname);
+                                    HttpContext.Session.SetString("email", email);
+                                    HttpContext.Session.SetString("phone", phone);
+                                    HttpContext.Session.SetString("address", address);
+                                    HttpContext.Session.SetString("role", role);
+                                    HttpContext.Session.SetString("created_at", created_at);
+
+                                    // the user is authenticated successfully => redirect to the home page
+                                    Response.Redirect("/");
+
+                                }
+
+
+                            }
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                errorMessage = ex.Message;
+                return;
+            }
+
+            errorMessage = "Wrong email or password";
+        }
+    }
+}
--- BestShop/Pages/Auth/Register.cshtml.cs
+++ BestShop/Pages/Auth/Register.cshtml.cs
@@ -1,60 +1,165 @@
+using BestShop.Myhelper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
 
 namespace BestShop.Pages.Auth
 {
+	[BindProperties]
     public class RegisterModel : PageModel
     {
-        [Required(ErrorMessage ="The First Name is required")]
-        public string FirstName { get; set; } = "";
+		[Required(ErrorMessage = "The First Name is required")]
+		public string Firstname { get; set; } = "";
 
-        [Required(ErrorMessage = "The Last Name is required")]
-        public string LastName { get; set; } = "";
+		[Required(ErrorMessage = "The Last Name is required")]
+		public string Lastname { get; set; } = "";
 
-        [Required(ErrorMessage = "The Email is required")]
-        public string Email { get; set; } = "";
+		[Required(ErrorMessage = "The Email is required"), EmailAddress]
+		public string Email { get; set; } = "";
 
-        [Required(ErrorMessage = "The Phone is required")]
-        public string? Phone { get; set; } = "";
+		public string? Phone { get; set; } = "";
 
-        [Required(ErrorMessage = "The Address is required")]
-        public string Address { get; set; } = "";
+		[Required(ErrorMessage = "The Address is required")]
+		public string Address { get; set; } = "";
 
-        [Required(ErrorMessage = " Password is required")]
-        [StringLength(50,ErrorMessage ="Password must be between 5 and 50 characters",MinimumLength =5)]
-        public string Password { get; set; } = "";
+		[Required(ErrorMessage = "Password is required")]
+		[StringLength(50, ErrorMessage = "Password must be between 5 and 50 characters", MinimumLength = 5)]
+		public string Password { get; set; } = "";
 
-        [Required(ErrorMessage = " Confirm password is required")]
-        [Compare("Password",ErrorMessage ="Password and confirm password do not match")]
-        public string ConfirmPassword { get; set; } = "";
+		[Required(ErrorMessage = "Confirm Password is required")]
+		[Compare("Password", ErrorMessage = "Password and Confirm Password do not match")]
+		public string ConfirmPassword { get; set; } = "";
 
-        public string errorMessage = "";
-        public string successMessage = "";
-        public void OnGet()
-        {
-        }
 
-        public void OnPost() 
-        { 
-           if(!ModelState.IsValid)
-            {
-                errorMessage = "Data validation failed";
-                return;
+		public string errorMessage = "";
+		public string successMessage = "";
+
+		public override void OnPageHandlerExecuting(PageHandlerExecutingContext context)
+		{
+			base.OnPageHandlerExecuting(context);
+
+			if(HttpContext.Session.GetString("role") != null)
+			{
+				//the user already authenticated => redirect the user to the home page
+				context.Result = new RedirectResult("/");
+			}
+		}
+
+		public void OnGet()
+		{
+		}
+
+		public void OnPost()
+		{
+			if (!ModelState.IsValid)
+			{
+				errorMessage = "Data validation failed";
+				return;
+			}
+
+			// successfull data validation
+			if (Phone == null) Phone = "";
+
+			//add the user details to the database
+			string connectionString = "Data Source=DESKTOP-7T0EOMO;database=bestshop;Integrated Security=True;";
+			try
+			{
+				using(SqlConnection connection=new SqlConnection(connectionString))
+				{
+					connection.Open();
+					string sql = "INSERT INTO users " +
+						"(firstname,lastname,email,phone,address,password,role) VALUES " +
+						"(@firstname,@lastname,@email,@phone,@address,@password,'client');";
+
+					var passwordHasher = new PasswordHasher<IdentityUser>();
+					string hashedPassword = passwordHasher.HashPassword(new IdentityUser(), Password);
+
+					using(SqlCommand  command =new SqlCommand (sql,connection))
+					{
+						command.Parameters.AddWithValue("@firstname", Firstname);
+						command.Parameters.AddWithValue("@lastname", Lastname);
+						command.Parameters.AddWithValue("@email", Email);
+						command.Parameters.AddWithValue("@phone", Phone);
+						command.Parameters.AddWithValue("@address", Address);
+						command.Parameters.AddWithValue("@password", hashedPassword);
+
+						command.ExecuteNonQuery();
+					}
+					  
+				}
+			}
+			catch(Exception ex)
+			{
+				if (ex.Message.Contains(Email))
+				{
+					errorMessage = "Email Id already Exists";
+				}
+				else
+				{
+					errorMessage = ex.Message;
+				}
+				return;
             }
+			//send confirmation email to the user
+			string username = Firstname + " " + Lastname;
+			string subject = "Account created successfully";
+			string message = "Dear " + username + ",\n\n" +
+				"Your account has been created successfully..\n\n" +
+				"Best Regards";
+			EmailSender.SendEmail(Email, username, subject, message).Wait();
 
-            //successfull data validation
-            if (Phone == null) Phone = "";
+			//initiate the authenticated session=> add the user details to the session data
+			try
+			{
+				using (SqlConnection connection = new SqlConnection(connectionString))
+				{
+					connection.Open();
+					string sql = "SELECT * FROM users WHERE email=@email";
 
-            //add the user details to the database
+					using (SqlCommand command = new SqlCommand(sql, connection))
+					{
+						command.Parameters.AddWithValue("@email", Email);
 
-            //send confirmation email to the user
+						using (SqlDataReader reader = command.ExecuteReader())
+						{
+							if (reader.Read())
+							{
+								int id = reader.GetInt32(0);
+								string firstname = reader.GetString(1);
+								string lastname = reader.GetString(2);
+								string email = reader.GetString(3);
+								string phone = reader.GetString(4);
+								string address = reader.GetString(5);
+								//string hashedPassword = reader.GetString(6);
+								string role = reader.GetString(7);
+								string created_at = reader.GetDateTime(8).ToString("MM/dd/yyyy");
 
-            //initiate the authenticated session=> add the user details to the session data
+								HttpContext.Session.SetInt32("id", id);
+								HttpContext.Session.SetString("firstname", firstname);
+								HttpContext.Session.SetString("lastname", lastname);
+								HttpContext.Session.SetString("email", email);
+								HttpContext.Session.SetString("phone", phone);
+								HttpContext.Session.SetString("address", address);
+								HttpContext.Session.SetString("role", role);
+								HttpContext.Session.SetString("created_at", created_at);
+							}
+						}
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				errorMessage = ex.Message;
+				return;
+			}
+			successMessage = "Account created successfully..";
 
-            successMessage = "Account created successfully..";
-
+			//redirect to the home page
+			Response.Redirect("/");
         }
     }
 }
